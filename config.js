10d4b7306b0da5535bedd0f92a964623dd7c4ba8
Rainbow <rahilazayan@gmail.com>
2024-10-14 00:01:00 +0800

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "gb0kCJyT#jpwGmM4u-vc_7S6uyrnVQJje29mm0f7Tp1KH2SyMBZc",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
